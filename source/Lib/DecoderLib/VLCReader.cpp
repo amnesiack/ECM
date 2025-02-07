a0147c5729047060ae6f52f28ffdd9eb8156725c
yo-kidani <yo-kidani@kddi.com>
2025-02-07 11:48:46 +0900

    Fix AJ0274

--- source/Lib/DecoderLib/VLCReader.cpp
+++ source/Lib/DecoderLib/VLCReader.cpp
@@ -2797,18 +2797,21 @@ void HLSyntaxReader::parseSPS(SPS* pcSPS)
 #endif
 
 #if JVET_AG0164_AFFINE_GPM
-      if (pcSPS->getUseAffine() && pcSPS->getMaxNumGeoCand() != 0 && pcSPS->getMaxNumAffineMergeCand() >= 3)
+      if (pcSPS->getUseAffine())
       {
-        READ_UVLC(uiCode, "max_num_aff_merge_cand_minus_max_num_gpm_aff_cand");
-        pcSPS->setMaxNumGpmAffCand((uint32_t)(pcSPS->getMaxNumAffineMergeCand() - uiCode));
-      }
+        if (pcSPS->getMaxNumGeoCand() != 0 && pcSPS->getMaxNumAffineMergeCand() >= 3)
+        {
+          READ_UVLC(uiCode, "max_num_aff_merge_cand_minus_max_num_gpm_aff_cand");
+          pcSPS->setMaxNumGpmAffCand((uint32_t)(pcSPS->getMaxNumAffineMergeCand() - uiCode));
+        }
 #if JVET_AJ0274_GPM_AFFINE_TM
-      if (pcSPS->getMaxNumGpmAffCand() > 0)
-      {
-        READ_UVLC(uiCode, "max_num_gpm_aff_tm_cand");
-        pcSPS->setMaxNumGpmAffTmCand((uint32_t)uiCode);
-      }
+        if (pcSPS->getMaxNumGpmAffCand() > 0)
+        {
+          READ_UVLC(uiCode, "max_num_gpm_aff_tm_cand");
+          pcSPS->setMaxNumGpmAffTmCand((uint32_t)uiCode);
+        }
 #endif
+      }
 #endif
 
 #if JVET_AA0132_CONFIGURABLE_TM_TOOLS && JVET_W0097_GPM_MMVD_TM && TM_MRG
