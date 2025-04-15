af86895c9e7d4f2148f58c660b7f7c63bd27aa8f
xlxiangli <xlxiangli@google.com>
2025-04-15 03:47:00 +0000

    Fix clang 19 compiling issues

--- source/Lib/EncoderLib/InterSearch.h
+++ source/Lib/EncoderLib/InterSearch.h
@@ -831,8 +831,6 @@ private:
 #endif
 #if JVET_AL0181_ASBT
   Pel             **m_interasbt;
-  bool            *m_asbtLineColumn;
-  int32_t         *m_enerBuffer;
 #endif
 #if JVET_AE0169_BIPREDICTIVE_IBC
   Distortion      m_bestBvpSADHADCost;
